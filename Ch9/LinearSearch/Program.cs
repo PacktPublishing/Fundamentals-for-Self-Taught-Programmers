0260620fe5af1117b5df4edbb5cbd851990a1567
Jasmine Greenaway <1414307+paladique@users.noreply.github.com>
2022-12-29 04:49:38 -0500

    chapter 9 examples

---
+++ Ch9/LinearSearch/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] testArray = {13, 55, 0, -4, 80, 7, 20};  
+int searchValue = 0;
+
+for (int index = 0; index < testArray.Length; index++)
+{
+    if(searchValue == testArray[index])
+    {
+        Console.WriteLine("Value found!");
+    }
+}
+
+Console.WriteLine("Value not found!");
