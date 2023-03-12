eef2e5170be6fa0de6ee727b65631929e97a9542
Jasmine Greenaway <paladique@users.noreply.github.com>
2023-03-12 17:47:10 +0000

    ch 6 code

---
+++ Chapter 6/MyBudget/Program.cs
@@ -0,0 +1,14 @@
+﻿decimal balance = 75.45M;
+string balanceMessage = "The current balance is: $";
+Console.WriteLine(balanceMessage + balance);
+balance -= 40;
+Console.WriteLine(balanceMessage + balance);
+balance -= 5;
+Console.WriteLine(balanceMessage + balance);
+
+decimal bookPrice = 25;
+Console.WriteLine(balance == bookPrice);
+Console.WriteLine(balance != bookPrice);
+Console.WriteLine(balance <= bookPrice);
+Console.WriteLine(balance >= bookPrice);
+Console.WriteLine(balance > bookPrice);
